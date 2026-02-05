6559e3dd6311f33f1c8ff7621317781345b3af20
Gannetzt <gannetzt@gmail.com>
2026-02-05 20:39:35 +0530

    Ensure all changes are pushed for Vercel deployment

--- my-react-app/src/components/ProductsOverview.tsx
+++ my-react-app/src/components/ProductsOverview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import { categories, solutions } from '../data/products';
+import { solutions } from '../data/products';
 import './ProductsOverview.css';
 
 const ProductsOverview: React.FC = () => {
